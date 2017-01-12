4185415fa2c0230f4d36660cde18d15709a6134e
guldam <guldam@kaist.ac.kr>
2017-01-12 16:37:50 +0900

    My Django Girls app, 첫번째 커밋

---
+++ .gitignore.py
@@ -0,0 +1,7 @@
+Python 3.5.3rc1 (v3.5.3rc1:de530d7f21c0, Jan  2 2017, 06:41:25) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> *.pyc
+__pycache__
+myvenv
+db.sqlite3
+.DS_Store
